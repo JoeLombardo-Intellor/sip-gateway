ab83377c07f2c3cd4ef3dabee42f5966d39fdba2
JoeLombardo-Intellor <135155225+JoeLombardo-Intellor@users.noreply.github.com>
2025-03-26 17:04:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const startGateway = require('./src/index.js');
 
 startGateway({
   port: 3000,
+  host: '0.0.0.0',      
   portSIP: 5060,
   onListen: () => {
     console.log('Gateway listening on ports 5060 (SIP) and 3000 (WS)');
