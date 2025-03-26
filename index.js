920303652c436a290244ccc6c073d5726555f7ca
JoeLombardo-Intellor <135155225+JoeLombardo-Intellor@users.noreply.github.com>
2025-03-26 17:04:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const startGateway = require('./src/index.js');
 
 startGateway({
   port: 3000,
-  host: '0.0.0.0',      
+  host: '0.0.0.0',           // Required for Render
   portSIP: 5060,
   onListen: () => {
     console.log('Gateway listening on ports 5060 (SIP) and 3000 (WS)');
+  },
+  onConnect: (socket) => {
+    console.log('New WebSocket connection');
   },
   onReceive: (data, stream) => {
     console.log('Received data:', data.length);
@@ -14,4 +17,3 @@ startGateway({
     console.log('Sending data:', data.length);
   }
 });
-
